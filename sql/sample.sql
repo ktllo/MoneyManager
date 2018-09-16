78789e55c4fe119579a66a156ecf04eb95f0a7cb
LO Kam Tao Leo <leolo@leolo.org>
2018-09-16 09:55:43 +0800

    Added table for user and roles
    Signed-off-by: LO Kam Tao Leo <leolo@leolo.org>

--- sql/sample.sql
+++ sql/sample.sql
@@ -1,3 +1,5 @@
 -- Create default user root with password P@ssw0rd
 
-INSERT INTO user VALUES (NULL,'root','$2y$12$m3SGOkI16vVQja09Pwsiue/zKiJg6.sLyqDRAgs8mbOWn9zBhxE92','1970-01-01 00:00:00','Y');
+INSERT INTO user VALUES (1,'root','$2y$12$m3SGOkI16vVQja09Pwsiue/zKiJg6.sLyqDRAgs8mbOWn9zBhxE92','1970-01-01 00:00:00','Y');
+INSERT INTO role VALUES( 'ADMIN' , 'System administrator');
+INSERT INTO userRole VALUES (1,'ADMIN');
