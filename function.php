2724c2101621fa17f7d491364d6f1441fc5dd838
LO Kam Tao Leo <leolo@leolo.org>
2018-09-29 16:10:38 +0800

    Changed the current log usage

--- function.php
+++ function.php
@@ -16,12 +16,6 @@ function getDBConn(){
 return new PDO("mysql:host=".DB_PATH.";dbname=".DB_NAME, DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 }
 
-function getLog($db){
-return $db->prepare('INSERT INTO audit_log VALUES(NULL,?,NOW(),?)');
-}
 
-function doLog($log, $tag, $msg){
-	$log->execute(array($tag,$msg));
-}
 
 
