900e463aba0a1d4b85f822200bedf3f7415fb174
LO Kam Tao Leo <leolo@leolo.org>
2018-09-29 08:26:51 +0800

    Updated sample config file * Changed suggested method to create node id * Will terminate if user has not finish reading the config file

--- config.sample.php
+++ config.sample.php
@@ -7,6 +7,8 @@ define('DB_PASS','password');
 define('DB_NAME','moneymanager');
 #The random ID can be generated using the output of the following link
 #Please remove space from he generated output.
-#https://www.random.org/cgi-bin/randbyte?nbytes=7&format=h
-define('NODE_ID','9c5109155831fe');
+#https://www.random.org/strings/?num=1&len=14&digits=on&loweralpha=on&unique=on&format=plain&rnd=new
+#Please remove the follow line after replacing the NODE_ID
+die('Please read though the config.php');
+define('NODE_ID','71f7an6z0mnf8x');
 define('PASSWORD_COST',12);
