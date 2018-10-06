d3e99bb015bee0b17ceba6082381cc7b75f6c765
LO Kam Tao Leo <leolo@leolo.org>
2018-10-06 16:13:33 +0800

    Modified sample config file

--- config.sample.php
+++ config.sample.php
@@ -7,8 +7,18 @@ define('DB_PASS','password');
 define('DB_NAME','moneymanager');
 #The random ID can be generated using the output of the following link
 #Please remove space from he generated output.
-#https://www.random.org/strings/?num=1&len=14&digits=on&loweralpha=on&unique=on&format=plain&rnd=new
-#Please remove the follow line after replacing the NODE_ID
+#https://www.random.org/strings/?num=2&len=14&digits=on&loweralpha=on&unique=on&format=plain&rnd=new
+#Please remove the follow line after replacing the value of NODE_ID and BNODE_ID
 die('Please read though the config.php');
 define('NODE_ID','71f7an6z0mnf8x');
+define('BNODE_ID','yw113hqngxd7j9');
+
+#The higher the password cost, the long it takes for login, but it will slow
+#down the brute force attacks
 define('PASSWORD_COST',12);
+
+#The API key for Aplha Vantage
+#Please obtain the api key from https://www.alphavantage.co/support/#api-key
+#Please remove the follow line after replacing the value of AV_API_KEY
+die('Please read though the config.php');
+define('AV_API_KEY','demo');
