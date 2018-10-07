0f5243fbff4d762a8a91433d321782058dca7754
LO Kam Tao Leo <leolo@leolo.org>
2018-10-07 15:21:39 +0800

    Main page layout changes

--- main.php
+++ main.php
@@ -1,1 +1,37 @@
-<a href="t_logout.php">Log out</a>
+<?php
+if(!defined('SESS_CHK')){
+	header('Location: .');
+	return;
+}
+include_once 'config.php';
+include_once 'function.php';
+?><!DOCTYPE html>
+<html>
+	<head>
+		<title>Money Manager</title>
+		<style>
+div.func{
+	background-color: red;
+	position: absolute;
+	left:0;
+	width: 20%;
+	top: 0;
+	height: 100%;
+	overflow: auto;
+}
+div.main{
+	background-color: blue;
+	position: absolute;
+	right: 0;
+	width: 80%;
+	top: 0;
+	height: 100%;
+	overflow: auto;
+}
+		</style>
+	</head>
+	<body>
+		<div class="func"> </div>
+		<div class="main"> </div>
+	</body>
+</html>
